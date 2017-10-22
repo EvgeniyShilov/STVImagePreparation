2f61f7b0ba079f75d256e97c050924a644075129
EvgeniyShilov <eugshilov@gmail.com>
2017-10-22 14:00:09 +0300

    made code useful

--- src/App.java
+++ src/App.java
@@ -3,15 +3,24 @@ import java.util.List;
 
 public class App {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         if (args.length == 0) throw new IllegalArgumentException("No path in the args");
         final String path = args[0];
-        Image input = new Image(path + "input.png");
-        Image bicolour = input.apply(new BicolourFilter(128)).save(path + "bicolour.png");
-        Image median = bicolour.apply(MedianFilter.getInstance()).save(path + "median.png");
-        List<ImageArea> areas = ImageArea.getAreas(median, 50);
-        int i = 0;
-        for (ImageArea area : areas)
-            area.toImage().resize(256).save(path + "areas\\" + i++ + ".png");
+        for (int i = 0; i < 40; i++) {
+            try {
+                System.out.println("Processing " + i + " image");
+                Image input = new Image(path + "sources\\" + i + ".png");
+                Image bicolour = input.apply(new BicolourFilter(136)).save(path + "output\\" + i + "\\bicolour.png");
+                System.out.println("Bicolour image was saved");
+                List<ImageArea> areas = ImageArea.getAreas(bicolour, 90);
+                System.out.println("Possible symbols count = " + areas.size());
+                int j = 0;
+                for (ImageArea area : areas)
+                    area.toImage().resize(256).save(path + "output\\" + i + "\\" + j++ + ".png");
+                System.out.println("Symbols were saved to " + i + " directory");
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }
